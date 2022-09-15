7079c8c94c2ba47e1573ca9b11d36375ae6dc271
BolivarCaro <bolivarcarobtk@gmail.com>
2022-09-15 00:09:05 -0500

    Metodo agrgar y mostrar municipios

---
+++ Torneo.App.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
+namespace Torneo.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+        static void Main(string[] args)
+        {
+        }
+    }
+}
