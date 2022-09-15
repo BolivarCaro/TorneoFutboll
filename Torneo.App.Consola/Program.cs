e8ab4723ec58ecfa8aa54918ad5c3c78127521b5
BolivarCaro <bolivarcarobtk@gmail.com>
2022-09-15 00:34:58 -0500

    Metodo agrgar y mostrar municipios

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -7,6 +7,46 @@ namespace Torneo.App.Consola
         private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
         static void Main(string[] args)
         {
+            #region Menu Inicio
+            Console.WriteLine("Selecione opcion a trabajar");             
+            Console.WriteLine("1. Municipios");
+            Console.WriteLine("2. Posicion");
+            Console.WriteLine("3. Jugador");
+            Console.WriteLine("4. Partido");
+            Console.WriteLine("5. Director Tecnico");
+            Console.WriteLine("6. Equipos");
+            //Console.ReadKey();
+            int selecionar = int.Parse(Console.ReadLine());
+            #endregion
+
+            if (selecionar == 1)
+            {
+                Console.WriteLine("1. Agregar Municipios");
+                Console.WriteLine("2. Mostrar Municipios");
+                selecionar = int.Parse(Console.ReadLine());
+
+                if (selecionar == 1)
+                {
+                    Console.WriteLine("Agregar Municipios");
+                    AddMunicipio();
+                }
+
+                if (selecionar == 2)
+                {
+                    Console.WriteLine("Mostrar Municipios");
+                    GetAllMunicipios();
+                    Console.ReadKey();
+                }
+
+            }
+        }
+
+        private static void GetAllMunicipios()
+        {
+            foreach (var municipio in _repoMunicipio.GetAllMunicipios())
+            {
+                Console.WriteLine(municipio.Id + " " + municipio.Nombre);
+            }
         }
     }
 }
