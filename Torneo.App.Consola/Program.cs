d2efa4fa67d6ee1da6edb93a7fda693ebae2c257
BolivarCaro <bolivarcarobtk@gmail.com>
2022-09-15 01:53:02 -0500

    Actualizado municipios y Dt

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -5,40 +5,89 @@ namespace Torneo.App.Consola
     class Program
     {
         private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+        private static IRepositorioDT _repoDT = new RepositorioDT();
+        private static IRepositorioEquipo _repoEquipo = new RepositorioEquipo();
+
         static void Main(string[] args)
         {
-            #region Menu Inicio
-            Console.WriteLine("Selecione opcion a trabajar");             
-            Console.WriteLine("1. Municipios");
-            Console.WriteLine("2. Posicion");
-            Console.WriteLine("3. Jugador");
-            Console.WriteLine("4. Partido");
-            Console.WriteLine("5. Director Tecnico");
-            Console.WriteLine("6. Equipos");
-            //Console.ReadKey();
-            int selecionar = int.Parse(Console.ReadLine());
-            #endregion
-
-            if (selecionar == 1)
+            int opcion = 0;
+            do
             {
-                Console.WriteLine("1. Agregar Municipios");
-                Console.WriteLine("2. Mostrar Municipios");
-                selecionar = int.Parse(Console.ReadLine());
-
-                if (selecionar == 1)
+                Console.WriteLine("1 Insertar Municipio");
+                Console.WriteLine("2. Insertar Director tecnico");
+                Console.WriteLine("3. Insertar Equipo");
+                Console.WriteLine("4. Mostrar Municipios");
+                Console.WriteLine("5. Mostrar DTs");
+                Console.WriteLine("6. Mostrar Equipos");
+                Console.WriteLine("0 Salir");
+                Console.WriteLine("Seleccione la opción correcta");
+                opcion = Int32.Parse(Console.ReadLine());
+                switch (opcion)
                 {
-                    Console.WriteLine("Agregar Municipios");
-                    AddMunicipio();
+                    case 1:
+                        AddMunicipio();
+                        break;
+                    case 2:
+                        AddDT();
+                        break;
+                    case 3:
+                        AddEquipo();
+                        break;
+                    case 4:
+                        GetAllMunicipios();
+                        break;
+                    case 5:
+                        GetAllDTs();
+                        break;
+                    case 6:
+                        GetAllEquipos();
+                        break;
                 }
+            } while (opcion != 0);
+        }
 
-                if (selecionar == 2)
-                {
-                    Console.WriteLine("Mostrar Municipios");
-                    GetAllMunicipios();
-                    Console.ReadKey();
-                }
+        private static void AddMunicipio()
+        {
+            Console.WriteLine("Ingrese el nombre del municipio");
+            string nombre = Console.ReadLine();
+            var municipio = new Municipio
+            {
+                Nombre = nombre,
+            };
+            _repoMunicipio.AddMunicipio(municipio);
+        }
 
-            }
+        private static void AddDT()
+        {
+            Console.WriteLine("Ingrese el nombre del DT");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el documento del DT");
+            string documento = Console.ReadLine();
+            Console.WriteLine("Ingrese el telefono del DT");
+            string telefono = Console.ReadLine();
+            var directorTecnico = new DirectorTecnico
+            {
+                Nombre = nombre,
+                Documento = documento,
+                Telefono = telefono,
+            };
+            _repoDT.AddDT(directorTecnico);
+        }
+
+        private static void AddEquipo()
+        {
+            Console.WriteLine("Ingrese el nombre del Equipo");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el id del Municipio");
+            int idMunicipio = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el id del DT");
+            int idDT = Int32.Parse(Console.ReadLine());
+
+            var equipo = new Equipo
+            {
+                Nombre = nombre,
+            };
+            _repoEquipo.AddEquipo(equipo, idMunicipio, idDT);
         }
 
         private static void GetAllMunicipios()
@@ -48,5 +97,24 @@ namespace Torneo.App.Consola
                 Console.WriteLine(municipio.Id + " " + municipio.Nombre);
             }
         }
+
+        private static void GetAllDTs()
+        {
+            foreach (var directorTecnico in _repoDT.GetAllDTs())
+            {
+                Console.WriteLine(directorTecnico.Id + " " + directorTecnico.Nombre);
+            }
+        }
+
+        private static void GetAllEquipos()
+        {
+            foreach (var equipo in _repoEquipo.GetAllEquipos())
+            {
+                Console.WriteLine(equipo.Nombre + " "
+                + equipo.Municipio.Nombre + " " + equipo.DirectorTecnico.Nombre);
+
+            }
+        }
+
     }
 }
